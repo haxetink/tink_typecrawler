067030298697c877edd5bc8426294b2279812fe0
Kevin Leung <kevinresol@gmail.com>
2017-03-28 18:10:12 +0800

    Organize a bit

--- tests/EnumAbstractTest.hx
+++ tests/EnumAbstractTest.hx
@@ -1,6 +1,11 @@
 package;
 
+import haxe.macro.Expr;
 import tink.unit.Assert.*;
+
+#if macro
+using tink.MacroApi;
+#end
 
 class EnumAbstractTest {
   public function new() {}
@@ -9,9 +14,20 @@ class EnumAbstractTest {
     return assert(run().join(',') == 'A,B,C');
   }
   macro static function run() {
-    return tink.typecrawler.Crawler.crawl(haxe.macro.Context.getType('EnumAbstract'), (macro null).pos, Gen).expr;
+    return tink.typecrawler.Crawler.crawl(haxe.macro.Context.getType('EnumAbstract'), (macro null).pos, new EnumAbstractGen()).expr;
   }
 }
+
+#if macro
+class EnumAbstractGen extends Gen {
+  override function wrap(placeholder:Expr, ct:ComplexType):Function
+    return placeholder.func([{name: 'value', type: ct}]);
+  override function string():Expr
+    return macro value;
+  override function enumAbstract(names:Array<String>, e:Expr):Expr
+    return macro $a{names.map(function(n) return macro $v{n})};
+}
+#end
 
 
 @:enum
--- tests/Gen.hx
+++ tests/Gen.hx
@@ -9,80 +9,24 @@ using tink.MacroApi;
 using tink.CoreApi;
 
 class Gen {
-  static public function wrap(placeholder:Expr, ct:ComplexType):Function {
-	  return placeholder.func([{name: 'value', type: ct}]);
-  }
-  
-  static public function nullable(e:Expr):Expr {
-	  return throw 'abstract';
-  }
-  
-  static public function string():Expr {
-	  return macro value;
-  }
-  
-  static public function float():Expr {
-	  return throw 'abstract';
-  }
-  
-  static public function int():Expr {
-	  return throw 'abstract';
-  }
-  
-  static public function dyn(e:Expr, ct:ComplexType):Expr {
-	  return throw 'abstract';
-  }
-  
-  static public function dynAccess(e:Expr):Expr {
-	  return throw 'abstract';
-  }
-  
-  static public function bool():Expr {
-	  return throw 'abstract';
-  }
-  
-  static public function date():Expr {
-	  return throw 'abstract';
-  }
-  
-  static public function bytes():Expr {
-	  return throw 'abstract';
-  }
-  
-  static public function anon(fields:Array<FieldInfo>, ct:ComplexType):Expr {
-	  return throw 'abstract';
-  }
-  
-  static public function array(e:Expr):Expr {
-	  return throw 'abstract';
-  }
-  
-  static public function map(k:Expr, v:Expr):Expr {
-	  return throw 'abstract';
-  }
-  
-  static public function enm(constructors:Array<EnumConstructor>, ct:ComplexType, pos:Position, gen:GenType):Expr {
-	  return throw 'abstract';
-  }
-  
-  static public function enumAbstract(names:Array<String>, e:Expr):Expr {
-	  return macro $a{names.map(function(n) return macro $v{n})};
-  }
-  
-  static public function rescue(t:Type, pos:Position, gen:GenType):Option<Expr> {
-	  return throw 'abstract';
-  }
-  
-  static public function reject(t:Type):String {
-	  return throw 'abstract';
-  }
-  
-  static public function shouldIncludeField(c:ClassField, owner:Option<ClassType>):Bool {
-    return true;
-  }
-  
-  static public function drive(type:Type, pos:Position, gen:Type->Position->Expr):Expr {
-    return gen(type, pos);
-  }
-  
+  public function new() {}
+  public function wrap(placeholder:Expr, ct:ComplexType):Function return throw 'abstract';
+  public function nullable(e:Expr):Expr return throw 'abstract';
+  public function string():Expr return throw 'abstract';
+  public function float():Expr return throw 'abstract';
+  public function int():Expr return throw 'abstract';
+  public function dyn(e:Expr, ct:ComplexType):Expr return throw 'abstract';
+  public function dynAccess(e:Expr):Expr return throw 'abstract';
+  public function bool():Expr return throw 'abstract';
+  public function date():Expr return throw 'abstract';
+  public function bytes():Expr return throw 'abstract';
+  public function anon(fields:Array<FieldInfo>, ct:ComplexType):Expr return throw 'abstract';
+  public function array(e:Expr):Expr return throw 'abstract';
+  public function map(k:Expr, v:Expr):Expr return throw 'abstract';
+  public function enm(constructors:Array<EnumConstructor>, ct:ComplexType, pos:Position, gen:GenType):Expr return throw 'abstract';
+  public function enumAbstract(names:Array<String>, e:Expr):Expr return throw 'abstract';
+  public function rescue(t:Type, pos:Position, gen:GenType):Option<Expr> return throw 'abstract';
+  public function reject(t:Type):String return throw 'abstract';
+  public function shouldIncludeField(c:ClassField, owner:Option<ClassType>):Bool return true;
+  public function drive(type:Type, pos:Position, gen:Type->Position->Expr):Expr return gen(type, pos);
 }
